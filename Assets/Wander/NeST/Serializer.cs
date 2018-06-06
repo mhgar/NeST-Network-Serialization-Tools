ac2b3c8884717b20302938eeee15978ca5f7ed56
Maxwell <maxwellhunter@protonmail.com>
2018-06-07 05:12:18 +1200

    Added error checking

--- Assets/Wander/NeST/Serializer.cs
+++ Assets/Wander/NeST/Serializer.cs
@@ -1,15 +1,30 @@
+using System;
+
 namespace Wander.NeST
 {
   public class Serializer
   {
     byte[] data;
     int writeHead = 0;
+    int dataLength;
 
-    public Serializer(byte[] array, int startIndex = 0)
+    public Serializer(byte[] array, int startIndex = 0, int length = -1)
     {
       // Throw for errors
+      if (array == null) 
+        throw new ArgumentNullException("array");
+
+      if (length > array.Length) 
+        throw new ArgumentOutOfRangeException("length");
+
+      if (startIndex > length - 1) 
+        throw new ArgumentOutOfRangeException("startIndex");
+
       writeHead = startIndex;
       data = array;
+
+      // If length is default value, use size of the array instead.
+      dataLength = (length < 0 ? data.Length : length);
     }
     // Don't need new() here; just for parity with Deserialize()
     public void Serialize<T>(T structure) where T : ISerializable, new()
@@ -19,6 +34,7 @@ namespace Wander.NeST
 
     public void SerializeUsing<T>(T value, Writers.Writer<T> writer)
     {
+      // Allow thrown exceptions to fall through into the caller.
       writeHead += writer(value, data, writeHead);
     }
   }
