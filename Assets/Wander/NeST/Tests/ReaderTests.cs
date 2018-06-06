26431769c57d493e5ce169d019365eb368220084
Maxwell <maxwellhunter@protonmail.com>
2018-06-07 05:45:34 +1200

    Fixed and rewrote tests to match the switch to throwing

--- Assets/Wander/NeST/Tests/ReaderTests.cs
+++ Assets/Wander/NeST/Tests/ReaderTests.cs
@@ -7,124 +7,99 @@ using Wander.NeST;
 
 public class ReadersTests 
 {
-    [TestCase(new byte[] { 1, 6 }, 6, 1, 1)] // Read 6 from position 1
-    [TestCase(new byte[] { }, 0, 0, 1)] // Read nothing from out of bounds
-    public void ReadByte(byte[] array, byte expectedValue, int expectedRead, int startIndex = 0)
+    [TestCase(new byte[] { 1, 6 }, 6, 1)] // Read 6 from position 1
+    public void ReadByte(byte[] array, byte expectedValue, int startIndex = 0)
     {
         byte value;
-        var read = Readers.ReadByte(out value, array, startIndex);
-
-        Assert.AreEqual(read, expectedRead, "Read wrong number of bytes");
+        Readers.ReadByte(out value, array, startIndex);
         Assert.AreEqual(value, expectedValue, "Read wrong value: " + value + " != " + expectedValue);
     }
 
-    [TestCase(new byte[] { 1, 6 }, 6, 1, 1)] // Read 6 from position 1
-    [TestCase(new byte[] { }, 0, 0, 1)] // Read nothing from out of bounds
-    public void ReadSByte(byte[] array, sbyte expectedValue, int expectedRead, int startIndex = 0)
+    [TestCase(new byte[] { 1, 6 }, 6, 1)] // Read 6 from position 1
+    public void ReadSByte(byte[] array, sbyte expectedValue, int startIndex = 0)
     {
         sbyte value;
-        var read = Readers.ReadSByte(out value, array, startIndex);
-
-        Assert.AreEqual(read, expectedRead, "Read wrong number of bytes");
+        Readers.ReadSByte(out value, array, startIndex);
         Assert.AreEqual(value, expectedValue, "Read wrong value: " + value + " != " + expectedValue);
     }
 
-    [TestCase(new byte[] { 0x00, 0xAB, 0x00 }, 0x00AB, 2, 1)]
-    [TestCase(new byte[] { }, 0, 0, 1)] // Read nothing from out of bounds
-    public void ReadShort(byte[] array, long expectedValue, int expectedRead, int startIndex = 0)
+    [TestCase(new byte[] { 0x00, 0xAB, 0x00 }, 0x00AB, 1)]
+    public void ReadShort(byte[] array, long expectedValue, int startIndex = 0)
     {
         short value;
-        var read = Readers.ReadShort(out value, array, startIndex);
-
-        Assert.AreEqual(read, expectedRead, "Read wrong number of bytes");
+        Readers.ReadShort(out value, array, startIndex);
         Assert.AreEqual(value, expectedValue, "Read wrong value: " + value + " != " + expectedValue);
     }
 
-    [TestCase(new byte[] { 0x00, 0xAB, 0x00 }, 0x00AB, 2, 1)]
-    [TestCase(new byte[] { }, 0, 0, 1)] // Read nothing from out of bounds
-    public void ReadUShort(byte[] array, long expectedValue, int expectedRead, int startIndex = 0)
+    [TestCase(new byte[] { 0x00, 0xAB, 0x00 }, 0x00AB, 1)]
+
+    public void ReadUShort(byte[] array, long expectedValue, int startIndex = 0)
     {
         ushort value;
-        var read = Readers.ReadUShort(out value, array, startIndex);
-
-        Assert.AreEqual(read, expectedRead, "Read wrong number of bytes");
+        Readers.ReadUShort(out value, array, startIndex);
         Assert.AreEqual(value, expectedValue, "Read wrong value: " + value + " != " + expectedValue);
     }
 
-    [TestCase(new byte[] { 0x00, (byte) 'a', 0x00 }, (long) 'a', 2, 1)]
-    [TestCase(new byte[] { }, 0, 0, 1)] // Read nothing from out of bounds
-    public void ReadChar(byte[] array, long expectedValue, int expectedRead, int startIndex = 0)
+    [TestCase(new byte[] { 0x00, (byte) 'a', 0x00 }, (long) 'a', 1)]
+
+    public void ReadChar(byte[] array, long expectedValue, int startIndex = 0)
     {
         char value;
-        var read = Readers.ReadChar(out value, array, startIndex);
-
-        Assert.AreEqual(read, expectedRead, "Read wrong number of bytes");
+        Readers.ReadChar(out value, array, startIndex);
         Assert.AreEqual(value, expectedValue, "Read wrong value: " + value + " != " + expectedValue);
     }
 
-    [TestCase(new byte[] { 0x00, 0xAB, 0xAB, 0x00, 0x00 }, 0x0000ABAB, 4, 1)]
-    [TestCase(new byte[] { }, 0, 0, 1)] // Read nothing from out of bounds
-    public void ReadInt(byte[] array, long expectedValue, int expectedRead, int startIndex = 0)
+    [TestCase(new byte[] { 0x00, 0xAB, 0xAB, 0x00, 0x00 }, 0x0000ABAB, 1)]
+
+    public void ReadInt(byte[] array, long expectedValue, int startIndex = 0)
     {
         int value;
-        var read = Readers.ReadInt(out value, array, startIndex);
-
-        Assert.AreEqual(read, expectedRead, "Read wrong number of bytes");
+        Readers.ReadInt(out value, array, startIndex);
         Assert.AreEqual(value, expectedValue, "Read wrong value: " + value + " != " + expectedValue);
     }
 
-    [TestCase(new byte[] { 0x00, 0xAB, 0xAB, 0x00, 0x00 }, 0x0000ABAB, 4, 1)]
-    [TestCase(new byte[] { }, 0, 0, 1)] // Read nothing from out of bounds
-    public void ReadUInt(byte[] array, long expectedValue, int expectedRead, int startIndex = 0)
+    [TestCase(new byte[] { 0x00, 0xAB, 0xAB, 0x00, 0x00 }, 0x0000ABAB, 1)]
+
+    public void ReadUInt(byte[] array, long expectedValue, int startIndex = 0)
     {
         uint value;
-        var read = Readers.ReadUInt(out value, array, startIndex);
-
-        Assert.AreEqual(read, expectedRead, "Read wrong number of bytes");
+        Readers.ReadUInt(out value, array, startIndex);
         Assert.AreEqual(value, expectedValue, "Read wrong value: " + value + " != " + expectedValue);
     }
 
-    [TestCase(new byte[] { 0x00, 0xAB, 0xAB, 0xAB, 0xAB, 0x00, 0x00, 0x00, 0x00 }, 0x00000000ABABABAB, 8, 1)]
-    [TestCase(new byte[] { }, 0, 0, 1)] // Read nothing from out of bounds
-    public void ReadLong(byte[] array, long expectedValue, int expectedRead, int startIndex = 0)
+    [TestCase(new byte[] { 0x00, 0xAB, 0xAB, 0xAB, 0xAB, 0x00, 0x00, 0x00, 0x00 }, 0x00000000ABABABAB, 1)]
+
+    public void ReadLong(byte[] array, long expectedValue, int startIndex = 0)
     {
         long value;
-        var read = Readers.ReadLong(out value, array, startIndex);
-
-        Assert.AreEqual(read, expectedRead, "Read wrong number of bytes");
+        Readers.ReadLong(out value, array, startIndex);
         Assert.AreEqual(value, expectedValue, "Read wrong value: " + value + " != " + expectedValue);
     }
 
-    [TestCase(new byte[] { 0x00, 0xAB, 0xAB, 0xAB, 0xAB, 0x00, 0x00, 0x00, 0x00 }, 0x00000000ABABABAB, 8, 1)]
-    [TestCase(new byte[] { }, 0, 0, 1)] // Read nothing from out of bounds
-    public void ReadULong(byte[] array, long expectedValue, int expectedRead, int startIndex = 0)
+    [TestCase(new byte[] { 0x00, 0xAB, 0xAB, 0xAB, 0xAB, 0x00, 0x00, 0x00, 0x00 }, 0x00000000ABABABAB, 1)]
+
+    public void ReadULong(byte[] array, long expectedValue, int startIndex = 0)
     {
         ulong value;
-        var read = Readers.ReadULong(out value, array, startIndex);
-
-        Assert.AreEqual(read, expectedRead, "Read wrong number of bytes");
+        Readers.ReadULong(out value, array, startIndex);
         Assert.AreEqual(value, expectedValue, "Read wrong value: " + value + " != " + expectedValue);
     }
 
-    [TestCase(new byte[] { 0x00, 0x00, 0xC0, 0x7F, 0x44, 0x00, 0x00, 0x00, 0x00 }, 1023f, 4, 1)]
-    [TestCase(new byte[] { }, 0, 0, 1)] // Read nothing from out of bounds
-    public void ReadFloat(byte[] array, float expectedValue, int expectedRead, int startIndex = 0)
+    [TestCase(new byte[] { 0x00, 0x00, 0xC0, 0x7F, 0x44, 0x00, 0x00, 0x00, 0x00 }, 1023f, 1)]
+
+    public void ReadFloat(byte[] array, float expectedValue, int startIndex = 0)
     {
         float value;
-        var read = Readers.ReadFloat(out value, array, startIndex);
-
-        Assert.AreEqual(read, expectedRead, "Read wrong number of bytes");
+        Readers.ReadFloat(out value, array, startIndex);
         Assert.AreEqual(value, expectedValue, "Read wrong value: " + value + " != " + expectedValue);
     }
 
-    [TestCase(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x26, 0x68, 0x65, 0x41 }, 11223344.0, 8, 1)]
-    [TestCase(new byte[] { }, 0, 0, 1)] // Read nothing from out of bounds
-    public void ReadDouble(byte[] array, double expectedValue, int expectedRead, int startIndex = 0)
+    [TestCase(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x26, 0x68, 0x65, 0x41 }, 11223344.0, 1)]
+
+    public void ReadDouble(byte[] array, double expectedValue, int startIndex = 0)
     {
         double value;
-        var read = Readers.ReadDouble(out value, array, startIndex);
-
-        Assert.AreEqual(read, expectedRead, "Read wrong number of bytes");
+        Readers.ReadDouble(out value, array, startIndex);
         Assert.AreEqual(value, expectedValue, "Read wrong value: " + value + " != " + expectedValue);
     }
 }
--- Assets/Wander/NeST/Tests/WritersTests.cs
+++ Assets/Wander/NeST/Tests/WritersTests.cs
@@ -18,125 +18,93 @@ public class WritersTests
     return true;
   }
 
-  [TestCase(6, new byte[] { 0, 6, 0, 0, 0, 0, 0, 0, 0 }, 1, 1)] // Write 6 from position 1
-  [TestCase(0, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 0, 128)] // Write nothing from of bounds
-  public void WriteByte(byte value, byte[] expectedArray, int expectedWrite, int startIndex = 0)
+  [TestCase(6, new byte[] { 0, 6, 0, 0, 0, 0, 0, 0, 0 }, 1)] // Write 6 from position 1
+  public void WriteByte(byte value, byte[] expectedArray, int startIndex = 0)
   {
     var array = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     var Write = Writers.WriteByte(value, array, startIndex);
-
-    Assert.AreEqual(Write, expectedWrite, "Write wrong number of bytes");
-
     Assert.IsTrue(CompareArray(array, expectedArray), "Array did not match expected array.");
   }
 
-  [TestCase(6, new byte[] { 0, 6, 0, 0, 0, 0, 0, 0, 0 }, 1, 1)] // Write 6 from position 1
-  [TestCase(0, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 0, 128)] // Write nothing from of bounds
-  public void WriteSByte(sbyte value, byte[] expectedArray, int expectedWrite, int startIndex = 0)
+  [TestCase(6, new byte[] { 0, 6, 0, 0, 0, 0, 0, 0, 0 }, 1)] // Write 6 from position 1
+  public void WriteSByte(sbyte value, byte[] expectedArray, int startIndex = 0)
   {
     var array = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     var Write = Writers.WriteSByte(value, array, startIndex);
-
-    Assert.AreEqual(Write, expectedWrite, "Write wrong number of bytes");
     Assert.IsTrue(CompareArray(array, expectedArray), "Array did not match expected array.");
   }
 
-  [TestCase('a', new byte[] { 0, (byte) 'a', 0, 0, 0, 0, 0, 0, 0 }, 2, 1)] // Write 6 from position 1
-  [TestCase('\0', new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 0, 128)] // Write nothing from of bounds
-  public void WriteChar(char value, byte[] expectedArray, int expectedWrite, int startIndex = 0)
+  [TestCase('a', new byte[] { 0, (byte) 'a', 0, 0, 0, 0, 0, 0, 0 }, 1)] // Write 6 from position 1
+  public void WriteChar(char value, byte[] expectedArray, int startIndex = 0)
   {
     var array = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     var Write = Writers.WriteChar(value, array, startIndex);
-
-    Assert.AreEqual(Write, expectedWrite, "Write wrong number of bytes");
     Assert.IsTrue(CompareArray(array, expectedArray), "Array did not match expected array.");
   }
 
-  [TestCase(0xAB, new byte[] { 0, 0xAB, 0, 0, 0, 0, 0, 0, 0 }, 2, 1)] // Write 6 from position 1
-  [TestCase(0, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 0, 128)] // Write nothing from of bounds
-  public void WriteShort(long value, byte[] expectedArray, int expectedWrite, int startIndex = 0)
+  [TestCase(0xAB, new byte[] { 0, 0xAB, 0, 0, 0, 0, 0, 0, 0 }, 1)] // Write 6 from position 1
+  public void WriteShort(long value, byte[] expectedArray, int startIndex = 0)
   {
     var array = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     var Write = Writers.WriteShort((short) value, array, startIndex);
 
-    Assert.AreEqual(Write, expectedWrite, "Write wrong number of bytes");
+
     Assert.IsTrue(CompareArray(array, expectedArray), "Array did not match expected array.");
   }
 
-  [TestCase(0xAB, new byte[] { 0, 0xAB, 0, 0, 0, 0, 0, 0, 0 }, 2, 1)] // Write 6 from position 1
-  [TestCase(0, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 0, 128)] // Write nothing from of bounds
-  public void WriteUShort(long value, byte[] expectedArray, int expectedWrite, int startIndex = 0)
+  [TestCase(0xAB, new byte[] { 0, 0xAB, 0, 0, 0, 0, 0, 0, 0 }, 1)] // Write 6 from position 1
+  public void WriteUShort(long value, byte[] expectedArray, int startIndex = 0)
   {
     var array = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     var Write = Writers.WriteUShort((ushort) value, array, startIndex);
-
-    Assert.AreEqual(Write, expectedWrite, "Write wrong number of bytes");
     Assert.IsTrue(CompareArray(array, expectedArray), "Array did not match expected array.");
   }
 
-  [TestCase(0xABAB, new byte[] { 0, 0xAB, 0xAB, 0, 0, 0, 0, 0, 0 }, 4, 1)] // Write 6 from position 1
-  [TestCase(0, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 0, 128)] // Write nothing from of bounds
-  public void WriteInt(long value, byte[] expectedArray, int expectedWrite, int startIndex = 0)
+  [TestCase(0xABAB, new byte[] { 0, 0xAB, 0xAB, 0, 0, 0, 0, 0, 0 }, 1)] // Write 6 from position 1
+  public void WriteInt(long value, byte[] expectedArray, int startIndex = 0)
   {
     var array = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     var Write = Writers.WriteInt((int) value, array, startIndex);
-
-    Assert.AreEqual(Write, expectedWrite, "Write wrong number of bytes");
     Assert.IsTrue(CompareArray(array, expectedArray), "Array did not match expected array.");
   }
 
-  [TestCase(1023f, new byte[] { 0, 0x00, 0xc0, 0x7f, 0x44, 0, 0, 0, 0 }, 4, 1)] // Write 6 from position 1
-  [TestCase(0, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 0, 128)] // Write nothing from of bounds
-  public void WriteFloat(float value, byte[] expectedArray, int expectedWrite, int startIndex = 0)
+  [TestCase(1023f, new byte[] { 0, 0x00, 0xc0, 0x7f, 0x44, 0, 0, 0, 0 }, 1)] // Write 6 from position 1
+  public void WriteFloat(float value, byte[] expectedArray, int startIndex = 0)
   {
     var array = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     var Write = Writers.WriteFloat(value, array, startIndex);
-
-    Assert.AreEqual(Write, expectedWrite, "Write wrong number of bytes");
     Assert.IsTrue(CompareArray(array, expectedArray), "Array did not match expected array.");
   }
 
-  [TestCase(0xABAB, new byte[] { 0, 0xAB, 0xAB, 0, 0, 0, 0, 0, 0 }, 4, 1)] // Write 6 from position 1
-  [TestCase(0, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 0, 128)] // Write nothing from of bounds
-  public void WriteUInt(long value, byte[] expectedArray, int expectedWrite, int startIndex = 0)
+  [TestCase(0xABAB, new byte[] { 0, 0xAB, 0xAB, 0, 0, 0, 0, 0, 0 }, 1)] // Write 6 from position 1
+  public void WriteUInt(long value, byte[] expectedArray, int startIndex = 0)
   {
     var array = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     var Write = Writers.WriteUInt((uint) value, array, startIndex);
-
-    Assert.AreEqual(Write, expectedWrite, "Write wrong number of bytes");
     Assert.IsTrue(CompareArray(array, expectedArray), "Array did not match expected array.");
   }
 
-  [TestCase(0xABABABAB, new byte[] { 0, 0xAB, 0xAB, 0xAB, 0xAB, 0, 0, 0, 0 }, 8, 1)] // Write 6 from position 1
-  [TestCase(0, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 0, 128)] // Write nothing from of bounds
-  public void WriteLong(long value, byte[] expectedArray, int expectedWrite, int startIndex = 0)
+  [TestCase(0xABABABAB, new byte[] { 0, 0xAB, 0xAB, 0xAB, 0xAB, 0, 0, 0, 0 }, 1)] // Write 6 from position 1
+  public void WriteLong(long value, byte[] expectedArray, int startIndex = 0)
   {
     var array = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     var Write = Writers.WriteLong(value, array, startIndex);
-
-    Assert.AreEqual(Write, expectedWrite, "Write wrong number of bytes");
     Assert.IsTrue(CompareArray(array, expectedArray), "Array did not match expected array.");
   }
 
-  [TestCase(0xABABABAB, new byte[] { 0, 0xAB, 0xAB, 0xAB, 0xAB, 0, 0, 0, 0 }, 8, 1)] // Write 6 from position 1
-  [TestCase(0, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 0, 128)] // Write nothing from of bounds
-  public void WriteULong(long value, byte[] expectedArray, int expectedWrite, int startIndex = 0)
+  [TestCase(0xABABABAB, new byte[] { 0, 0xAB, 0xAB, 0xAB, 0xAB, 0, 0, 0, 0 }, 1)] // Write 6 from position 1
+  public void WriteULong(long value, byte[] expectedArray, int startIndex = 0)
   {
     var array = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     var Write = Writers.WriteULong((ulong) value, array, startIndex);
-
-    Assert.AreEqual(Write, expectedWrite, "Write wrong number of bytes");
     Assert.IsTrue(CompareArray(array, expectedArray), "Array did not match expected array.");
   }
 
-  [TestCase(11223344.0, new byte[] { 0, 0, 0, 0, 0, 0x26, 0x68, 0x65, 0x41 }, 8, 1)] // Write 6 from position 1
-  [TestCase(0, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 0, 128)] // Write nothing from of bounds
-  public void WriteDouble(double value, byte[] expectedArray, int expectedWrite, int startIndex = 0)
+  [TestCase(11223344.0, new byte[] { 0, 0, 0, 0, 0, 0x26, 0x68, 0x65, 0x41 }, 1)] // Write 6 from position 1
+  public void WriteDouble(double value, byte[] expectedArray, int startIndex = 0)
   {
     var array = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     var Write = Writers.WriteDouble(value, array, startIndex);
-
-    Assert.AreEqual(Write, expectedWrite, "Write wrong number of bytes");
     Assert.IsTrue(CompareArray(array, expectedArray), "Array did not match expected array.");
   }
 }
